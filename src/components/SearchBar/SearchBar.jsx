5028dffb1aa548dafc84e0ec5987fb1c7816e722
jorgezerpa <jorgezerpacoder@gmail.com>
2022-02-19 19:23:57 -0400

    ready to build

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,16 @@
 import React,{useState, useContext} from 'react';
 import { SearchBarContext } from '../../App';
 
+import { useLocation } from 'react-router-dom';
+
 import useStyles from './styles';
 
 import getBookService from '../../Services/getBooksService';
 
 
-
 const SearchBar = () => {
     const classes = useStyles();
-    
+
     const context = useContext(SearchBarContext);
 
   const search = async (value) => {
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -15,17 +15,21 @@ import useStyles from './styles';
 const NavBar = ()=>{
     const classes=useStyles();
 
-    
+            //to delete old searchs
+    const reload = ()=> {
+        window.reload();
+    }
+
     return (
             <Toolbar>
                 <Grid container justifyContent='center' spacing={2} >
-                    <Grid item ><Typography component = {Link} to='/home' variant="body1" style={{color: '#fff', textDecoration:'none'}}>HOME</Typography> </Grid>
-                    <Grid item ><Typography component = {Link} to='/featured' variant="body1" className={classes.navbarItem}>Featured</Typography> </Grid>
-                    <Grid item ><Typography component = {Link} to='/development' variant="body1" className={classes.navbarItem}>Development</Typography> </Grid>
-                    <Grid item ><Typography component = {Link} to='/design' variant="body1" className={classes.navbarItem}>Design</Typography> </Grid>
-                    <Grid item ><Typography component = {Link} to='/beginner' variant="body1" className={classes.navbarItem}>Beginner</Typography></Grid>
-                    <Grid item ><Typography component = {Link} to='/frontend' variant="body1" className={classes.navbarItem}>Frontend</Typography> </Grid>
-                    <Grid item ><Typography component = {Link} to='/backend' variant="body1" className={classes.navbarItem}>Backend</Typography> </Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/home' variant="body1" style={{color: '#fff', textDecoration:'none'}}>HOME</Typography> </Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/featured' variant="body1" className={classes.navbarItem}>Featured</Typography> </Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/development' variant="body1" className={classes.navbarItem}>Development</Typography> </Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/design' variant="body1" className={classes.navbarItem}>Design</Typography> </Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/beginner' variant="body1" className={classes.navbarItem}>Beginner</Typography></Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/frontend' variant="body1" className={classes.navbarItem}>Frontend</Typography> </Grid>
+                    <Grid item ><Typography onClick={reload} component = {Link} to='/backend' variant="body1" className={classes.navbarItem}>Backend</Typography> </Grid>
                 </Grid>
             </Toolbar>
     )
